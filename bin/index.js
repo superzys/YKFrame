c58967cf2b54208fc62d0d642476d49993774045
zys <506982926@qq.com>
2019-01-22 01:24:53 +0800

    fgui 等接入

---
+++ bin/index.js
@@ -0,0 +1,22 @@
+/**
+ * 设置LayaNative屏幕方向，可设置以下值
+ * landscape           横屏
+ * portrait            竖屏
+ * sensor_landscape    横屏(双方向)
+ * sensor_portrait     竖屏(双方向)
+ */
+window.screenOrientation = "sensor_landscape";
+
+//-----libs-begin-----
+loadLib("libs/laya.core.js");
+loadLib("libs/laya.webgl.js");
+loadLib("libs/laya.ui.js");
+loadLib("libs/laya.physics.js");
+loadLib("libs/laya.html.js");
+
+loadLib("libs/rawinflate/rawinflate.min.js");
+loadLib("libs/fairygui/fairygui.min.js");
+loadLib("../protobuf/library/protobuf-library.js");
+loadLib("../protobuf/bundles/protobuf-bundles.js");
+//-----libs-end-------
+loadLib("js/bundle.js");
